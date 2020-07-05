8300b0d10a3c3a0ae3174075100083d634966b26
GBernardo10 <g_bernardo10@hotmail.com>
2020-07-05 20:09:01 -0300

    fix: path to keypair private and public keys

--- auth/src/config/signature.ts
+++ auth/src/config/signature.ts
@@ -2,11 +2,11 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const privateKey = fs.readFileSync(
-  path.resolve(__dirname, '..', '..', '.ssh/jwtRS256.key'),
+  path.resolve(__dirname, '..', '..', '..','.ssh/jwtRS256.key'),
   'utf-8',
 );
 
 export const publicKey = fs.readFileSync(
-  path.resolve(__dirname, '..', '..', '.ssh/jwtRS256.key.pub'),
+  path.resolve(__dirname, '..', '..', '..','.ssh/jwtRS256.key.pub'),
   'utf-8',
 );
